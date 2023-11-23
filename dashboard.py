a9c47a1a038bb96ea4f921361ef2ed94292001ff
IsaacBreen <mail@isaacbreen.com>
2023-11-23 16:11:30 +0800

    fix

--- dashboard.py
+++ dashboard.py
@@ -6,77 +6,85 @@ from dash.dependencies import Input, Output
 from sklearn.model_selection import train_test_split
 
 from main import DataGenerator, FeatureEngineering, ModelTraining, DriftDetection, drift_detection_report, colorize_p_values, humanize_column_names, metadata_data
-app = dash.Dash(__name__)
-data_gen = DataGenerator(
-    n_rows=100,
-    categories=["A", "B", "C"],
-    category_type=pd.CategoricalDtype(categories=["A", "B", "C"], ordered=True),
-    p=0.3,
-    loc1=10,
-    loc2=20,
-    scale1=10,
-    scale2=10
+
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+
+def generate_data():
+    data_gen = DataGenerator(
+        n_rows=100,
+        categories=["A", "B", "C"],
+        category_type=pd.CategoricalDtype(categories=["A", "B", "C"], ordered=True),
+        p=0.3,
+        loc1=10,
+        loc2=20,
+        scale1=10,
+        scale2=10
+        )
+    data = data_gen.generate_data()
+    text_features = ['verbatim_text']
+    categorical_features = ['category_feature']
+    boolean_features = ['bool_feature']
+    numerical_features = ['float_feature']
+    feat_eng = FeatureEngineering(
+        text_features=text_features,
+        categorical_features=categorical_features,
+        boolean_features=boolean_features,
+        numerical_features=numerical_features
     )
-data = data_gen.generate_data()
-text_features = ['verbatim_text']
-categorical_features = ['category_feature']
-boolean_features = ['bool_feature']
-numerical_features = ['float_feature']
-feat_eng = FeatureEngineering(
-    text_features=text_features,
-    categorical_features=categorical_features,
-    boolean_features=boolean_features,
-    numerical_features=numerical_features
-)
-data = feat_eng.fit_transform(data)
-model_training = ModelTraining()
-features = [col for col in data.columns if
-            data[col].dtype in [np.float64, np.float32, np.int64, np.int32, np.uint8, np.uint16, np.uint32, np.uint64, np.bool_] and col != 'is_systemic_risk']
-target = 'is_systemic_risk'
+    data = feat_eng.fit_transform(data)
+    model_training = ModelTraining()
+    features = [col for col in data.columns if
+                data[col].dtype in [np.float64, np.float32, np.int64, np.int32, np.uint8, np.uint16, np.uint32, np.uint64, np.bool_] and col != 'is_systemic_risk']
+    target = 'is_systemic_risk'
 
-X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=0, stratify=data[target])
-model_training.train_model(X_train, y_train)
-y_pred = model_training.predict(X_test)
-train_accuracy = model_training.evaluate(y_train, model_training.predict(X_train))
-test_accuracy = model_training.evaluate(y_test, y_pred)
-data_gen = DataGenerator(
-    n_rows=100,
-    categories=["A", "B", "C"],
-    category_type=pd.CategoricalDtype(categories=["A", "B", "C"], ordered=True),
-    p=0.4,
-    loc1=15,
-    loc2=20,
-    scale1=10,
-    scale2=10
-    )
-new_data = data_gen.generate_data()
-new_data = feat_eng.transform(new_data)
+    X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=0, stratify=data[target])
+    model_training.train_model(X_train, y_train)
+    y_pred = model_training.predict(X_test)
+    train_accuracy = model_training.evaluate(y_train, model_training.predict(X_train))
+    test_accuracy = model_training.evaluate(y_test, y_pred)
+    data_gen = DataGenerator(
+        n_rows=100,
+        categories=["A", "B", "C"],
+        category_type=pd.CategoricalDtype(categories=["A", "B", "C"], ordered=True),
+        p=0.4,
+        loc1=15,
+        loc2=20,
+        scale1=10,
+        scale2=10
+        )
+    new_data = data_gen.generate_data()
+    new_data = feat_eng.transform(new_data)
 
-drift_det = DriftDetection()
-drift_report = drift_detection_report(
-    data,
-    new_data,
-    drift_det,
-    feature_types={'numerical': numerical_features, 'categorical': categorical_features, 'boolean': boolean_features, 'textual': text_features}
-    )
+    drift_det = DriftDetection()
+    drift_report = drift_detection_report(
+        data,
+        new_data,
+        drift_det,
+        feature_types={'numerical': numerical_features, 'categorical': categorical_features, 'boolean': boolean_features, 'textual': text_features}
+        )
 
-humanize_column_names(drift_report)
-colorize_p_values(drift_report)
+    humanize_column_names(drift_report)
+    colorize_p_values(drift_report)
 
-def evaluate_on_new_datasets(model_training, datasets, features):
-    results = {}
-    for name, new_data in datasets.items():
-        new_data = feat_eng.transform(new_data)  # Ensure the new data is transformed using the same feature engineering
-        X_new = new_data[features]
-        y_new_pred = model_training.predict(X_new)
-        # Assuming the ground truth labels are available in the new datasets
-        y_new_true = new_data['is_systemic_risk']
-        accuracy = model_training.evaluate(y_new_true, y_new_pred)
-        results[name] = accuracy
-    return results
+    def evaluate_on_new_datasets(model_training, datasets, features):
+        results = {}
+        for name, new_data in datasets.items():
+            new_data = feat_eng.transform(new_data)  # Ensure the new data is transformed using the same feature engineering
+            X_new = new_data[features]
+            y_new_pred = model_training.predict(X_new)
+            # Assuming the ground truth labels are available in the new datasets
+            y_new_true = new_data['is_systemic_risk']
+            accuracy = model_training.evaluate(y_new_true, y_new_pred)
+            results[name] = accuracy
+        return results
 
-new_datasets = {'new_dataset1': data_gen.generate_data(), 'new_dataset2': data_gen.generate_data()}
-new_data_results = evaluate_on_new_datasets(model_training, new_datasets, features)
+    new_datasets = {'new_dataset1': data_gen.generate_data(), 'new_dataset2': data_gen.generate_data()}
+    new_data_results = evaluate_on_new_datasets(model_training, new_datasets, features)
+
+    return drift_report, train_accuracy, test_accuracy, model_training, X_train, X_test, y_train, y_test, new_data_results, data, new_data, features, drift_det, feat_eng
+
+drift_report, train_accuracy, test_accuracy, model_training, X_train, X_test, y_train, y_test, new_data_results, data, new_data, features, drift_det, feat_eng = generate_data()
+
 
 metadata_div = html.Div([
     html.H3("Metadata"),
@@ -91,18 +99,6 @@ app.layout = html.Div([
         dcc.Tab(label='Drift Detection', value='tab-drift'),
     ]),
     html.Div(id='tabs-content'),
-    html.Div(id='model-analysis-content', children=[
-        html.H3('Model Performance'),
-        dcc.Graph(id='model_performance_graph'),
-        html.H3('Feature Importance'),
-        dcc.Graph(id='feature_importance_graph')  # Placeholder Graph
-    ]),
-    html.Div(id='drift-detection-content', children=[
-        html.H3('Drift Report'),
-        dash_table.DataTable(id='drift_report_table'),
-        html.H3('Metadata'),
-        html.Pre(id='metadata_text')
-    ])
 ])
 
 @app.callback(
