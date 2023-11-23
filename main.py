e2f7d29e8caf5d3fff359127fc384007ddefa108
IsaacBreen <mail@isaacbreen.com>
2023-11-23 14:03:48 +0800

    move eg usage

--- main.py
+++ main.py
@@ -196,38 +196,6 @@ class DriftDetection:
         return z_statistic, p_value
 
 
-# Example Usage
-
-# Data Generation (assuming this is specific and remains as provided)
-data_gen = DataGenerator(n_rows=20, categories=["A", "B", "C"], category_type=pd.CategoricalDtype(categories=["A", "B", "C"], ordered=True))
-data = data_gen.generate_data()
-
-# Define feature types
-text_features = ['verbatim_text']
-categorical_features = ['category_feature']
-boolean_features = ['bool_feature']
-numerical_features = ['float_feature']
-
-# Feature Engineering
-feat_eng = FeatureEngineering(text_features=text_features, categorical_features=categorical_features, boolean_features=boolean_features, numerical_features=numerical_features)
-data = feat_eng.fit_transform(data)
-
-# Model Training
-model_training = ModelTraining()
-
-# Define features based on transformed data
-features = [col for col in data.columns if data[col].dtype in [np.float64, np.float32, np.int64, np.int32, np.uint8, np.uint16, np.uint32, np.uint64, np.bool_] and col != 'is_systemic_risk']
-target = 'is_systemic_risk'
-
-X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=0)
-model_training.train_model(X_train, y_train)
-y_pred = model_training.predict(X_test)
-print("Model Accuracy:", model_training.evaluate(y_test, y_pred))
-
-# New Data for Drift Detection
-new_data = data_gen.generate_data()
-new_data = feat_eng.transform(new_data)
-
 def drift_detection_report(original_data, new_data, drift_detector, feature_types):
     report = []
 
@@ -343,6 +311,44 @@ for line in metadata.split('\n'):
         metadata_data[key.strip()] = value.strip()
 
 if __name__ == "__main__":
+    # Example Usage
+
+    # Data Generation (assuming this is specific and remains as provided)
+    data_gen = DataGenerator(n_rows=20, categories=["A", "B", "C"], category_type=pd.CategoricalDtype(categories=["A", "B", "C"], ordered=True))
+    data = data_gen.generate_data()
+
+    # Define feature types
+    text_features = ['verbatim_text']
+    categorical_features = ['category_feature']
+    boolean_features = ['bool_feature']
+    numerical_features = ['float_feature']
+
+    # Feature Engineering
+    feat_eng = FeatureEngineering(
+        text_features=text_features,
+        categorical_features=categorical_features,
+        boolean_features=boolean_features,
+        numerical_features=numerical_features
+        )
+    data = feat_eng.fit_transform(data)
+
+    # Model Training
+    model_training = ModelTraining()
+
+    # Define features based on transformed data
+    features = [col for col in data.columns if
+                data[col].dtype in [np.float64, np.float32, np.int64, np.int32, np.uint8, np.uint16, np.uint32, np.uint64, np.bool_] and col != 'is_systemic_risk']
+    target = 'is_systemic_risk'
+
+    X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=0)
+    model_training.train_model(X_train, y_train)
+    y_pred = model_training.predict(X_test)
+    print("Model Accuracy:", model_training.evaluate(y_test, y_pred))
+
+    # New Data for Drift Detection
+    new_data = data_gen.generate_data()
+    new_data = feat_eng.transform(new_data)
+
     drift_det = DriftDetection()
     drift_report = drift_detection_report(data, new_data, drift_det, feature_types={'numerical': numerical_features, 'categorical': categorical_features, 'boolean': boolean_features, 'textual': text_features})
     humanize_column_names(drift_report)
